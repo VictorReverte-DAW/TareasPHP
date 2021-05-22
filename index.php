1e82b98a568d15c48423b0476e59154ea3f61d5e
VictorReverte-DAW <mevictor@hotmail.es>
2021-05-22 12:50:13 +0200

    Primera instantanea 22-05-2021

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+//Incluyo los archivos necesarios
+/*
+require("./modal/Tarea.php");
+require("./controller/TareaController.php");
+*/
+require_once("db/db.php");
+
+require_once("./controller/ProyectoController.php");
+
+
+//Instancio el controlador
+//$controllerTarea = new TareaController;
+//$controllerProyecto = new ProyectoController;
+
+//Ejecuto el método
+//$controllerTarea->index();
+//$controllerProyecto->index();
